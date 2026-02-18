0df40ec26393c90f0632f93ee271334b3e86495b
fmvaldezg <fmvaldezg@yahoo.com>
2026-02-18 11:17:57 -0500

    Clean up repository - remove unnecessary files

--- dist/assets/run-M0GrmhF0.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/run.png";export{s as _};
--- dist/assets/slidev/default-D6MDwSac.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-BgPB_APO.js";import{_ as r}from"../index-Bol0u4wA.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
